94d24f29a3ca403d61e952e177df571748a0af79
dlawhdgus <212whdgus@gmail.com>
2023-01-18 22:38:00 +0900

    sign-up controller edit

--- routes/controller.js
+++ routes/controller.js
@@ -3,46 +3,55 @@ const mysql_callback = require('../models/mysql')
 const session = require('express-session')
 const crypto = require('../modules/crypto')
 
-exports.index = (req,res) => {
+exports.index = (req, res) => {
     res.render('index')
 }
 
-exports.sign_up = (req,res) => {
+exports.sign_up = (req, res) => {
     res.render('sign-up')
 }
 
-exports.sign_up_logic = async (req,res) => {
+exports.sign_up_logic = async (req, res) => {
     const { id, pw, name, email } = req.body
-    
+
     const id_regex = /^[a-z|A-Z]+$/
     const pw_regex = /^[a-z|A-Z|0-9]+$/
     const email_regex = /^[a-z0-9]+@[a-z]+\.[a-z]+$/
     const UserFilter = {}
-    
+
     const check_id = await mongodb_callback.check_duplication_id(id)
 
-    if(check_id) {
+    if (check_id) {
         res.write("<script>alert('id is duplicate');history.back();</script>")
     } else {
-        if(id_regex.test(id) && pw_regex.test(pw) && email_regex.test(email)) {
+        if (id_regex.test(id) && pw_regex.test(pw) && email_regex.test(email)) {
 
             UserFilter.id = id
             UserFilter.pw = crypto.encodig(pw)
             UserFilter.name = name
             UserFilter.email = email
-    
+
             const SignUpUser = await mongodb_callback.SignUp(UserFilter)
+            const user_data = await mongodb_callback.check_duplication_id(id)
+
+            req.session.userdata = user_data._id
+            res.redirect('sign-in?id=' + user_data._id)
+
         } else {
-            if(id_regex.test(id)) {
-                if(pw_regex.test(pw)) {
-                    if(email_regex.test(email)) {
+            if (id_regex.test(id)) {
+                if (pw_regex.test(pw)) {
+                    if (email_regex.test(email)) {
                         UserFilter.id = id
                         UserFilter.pw = crypto.encodig(pw)
                         UserFilter.name = name
                         UserFilter.email = email
-    
+
                         const SignUpUser = await mongodb_callback.SignUp(UserFilter)
-                        res.redirect('sign-in')
+                        const user_data = await mongodb_callback.check_duplication_id(id)
+
+                        req.session.userdata = user_data._id
+                        res.redirect('sign-in?id=' + user_data._id)
+
                     } else {
                         res.write("<script>alert('check the email format');history.back();</script>")
                     }
@@ -61,6 +70,7 @@ exports.sign_up_logic = async (req,res) => {
 
 }
 
-exports.sign_in = (req,res) => {
-    res.render('sign-in')
+exports.sign_in = (req, res) => {
+    const { id } = req.query
+    res.render('sign-in', {data : `${id}`})
 }
